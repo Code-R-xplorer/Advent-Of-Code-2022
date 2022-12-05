1dc008d3ae2899b79e38dd259b56d36ab622d947
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2022-12-05 21:46:13 +0000

    Day 5 - In Progress

--- src/Day_5.py
+++ src/Day_5.py
@@ -1,6 +1,86 @@
 from utils import read_file
 
-values = read_file(5, str, True)
+values = read_file(5, str, True, False)
 
-print(values)
+stack_strings = []
+instruction_strings = []
+
+
+end_stack = False
+for value in values:
+    if end_stack:
+        instruction_strings.append(value)
+    elif value == '':
+        end_stack = True
+    else:
+        stack_strings.append(value)
+
+total_columns = stack_strings.pop()
+tmp = total_columns.split(' ')
+total_columns = int(tmp[len(tmp) - 1])
+# print(total_columns)
+
+stacks = []
+# print(stack_strings)
+for stack_string in stack_strings:
+    row = []
+    split_stack_string = stack_string.split(' ')
+    column_counter = 0
+    for s in split_stack_string:
+        if s == '':
+            column_counter += 1
+        if column_counter == 4:
+            row.append('')
+            column_counter = 0
+        if '[' in s:
+            row.append(s.strip('[]'))
+    if len(row) < total_columns:
+        for i in range(total_columns - len(row)):
+            row.append('')
+    stacks.append(row)
+
+print(stacks)
+
+print(instruction_strings)
+instructions = []
+for s in instruction_strings:
+    instruction = []
+    split = s.split(' ')
+    instruction.append(int(split[1]))
+    instruction.append(int(split[3]) - 1)
+    instruction.append(int(split[5]) - 1)
+    instructions.append(instruction)
+
+
+# for instruction in instructions:
+#     amount = instruction[0]
+#     start = instruction[1]
+#     end = instruction[2]
+#     for i in range(amount):
+#         crate = stacks[0][start]
+#         stacks[0][start] = ''
+#         stacks[0][end] = crate
+
+instruction = instructions[0]
+amount = instruction[0]
+start = instruction[1]
+end = instruction[2]
+for i in range(amount):
+    crate = stacks[0][start]
+    stacks[0][start] = ''
+    stacks[0][end] = crate
+
+print(stacks)
+
+instruction = instructions[1]
+amount = instruction[0]
+start = instruction[1]
+end = instruction[2]
+for i in range(amount):
+    crate = stacks[0][start]
+    stacks[0][start] = ''
+
+    stacks[0][end] = crate
+
+print(stacks)
 
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,19 @@
 import os
 
 
-def read_file(day: int, transform, use_test = False) -> list:
+def read_file(day: int, transform, use_test = False, strip_line = True) -> list:
     try:
         if use_test:
             with open(os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'Input_Files', f'day_{day}_test.txt')) as f:
-                return [transform(line.strip()) for line in f]
+                if strip_line:
+                    return [transform(line.strip()) for line in f]
+                else:
+                    return [transform(line.strip('\n')) for line in f]
         else:
             with open(os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'Input_Files', f'day_{day}.txt')) as f:
-                return [transform(line.strip()) for line in f]
+                if strip_line:
+                    return [transform(line.strip()) for line in f]
+                else:
+                    return [transform(line.strip('\n')) for line in f]
     except FileNotFoundError as e:
         print(e)
