1e4ce77d216ebe9d1a68aa41e5544380ac35e344
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2022-12-08 20:16:31 +0000

    Day 7 Started

--- src/Day_7.py
+++ src/Day_7.py
@@ -1,4 +1,31 @@
 from utils import read_file
+from dataclasses import dataclass, field
 
-values = read_file(7, str, False)
+values = read_file(7, str, True)
 
+
+@dataclass
+class Directory:
+    name: str = ""
+    contents: list = field(default_factory=list)
+
+
+@dataclass
+class File:
+    name: str
+    size: int
+
+
+root = Directory()
+
+
+for value in values:
+    line = value.split()
+    # User Command
+    if line[0] == '$':
+        if line[1] == 'cd':
+            if line[2] == '/':
+                root.name = '/'
+
+
+print(root)
