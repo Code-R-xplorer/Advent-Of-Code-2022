690770b8b1b7b888524d1584853c376b51effe56
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2022-12-09 10:39:43 +0000

    Day 8 - Part 2 Complete

--- src/Day_8.py
+++ src/Day_8.py
@@ -37,3 +37,49 @@ for row in range(len(grid)):
 
 print("Part 1: " + str(visible))
 # Part 1: 1809
+
+scenic_scores = []
+
+for row in range(len(grid)):
+    for column in range(len(grid[row])):
+        # Ignore Edges
+        if row == 0 or row == len(grid) - 1 or column == 0 or column == len(grid[row]) - 1:
+            continue
+        current_tree_height = grid[row][column]
+        current_column = get_column(grid, column)
+        current_row = grid[row]
+        up = current_column[0:row]
+        down = current_column[row + 1:]
+        left = current_row[0:column]
+        right = current_row[column + 1:]
+
+        up_score, down_score, left_score, right_score = 0, 0, 0, 0
+        for i in reversed(range(len(up))):
+            up_score += 1
+            if current_tree_height <= up[i]:
+                break
+
+        for i in range(len(down)):
+            down_score += 1
+            if current_tree_height <= down[i]:
+                break
+
+        for i in reversed(range(len(left))):
+            left_score += 1
+            if current_tree_height <= left[i]:
+                break
+
+        for i in range(len(right)):
+            right_score += 1
+            if current_tree_height <= right[i]:
+                break
+
+        scenic_score = up_score * left_score * right_score * down_score
+        scenic_scores.append(scenic_score)
+
+print("Part 2: " + str(max(scenic_scores)))
+# Part 2: 479400
+
+
+
+
